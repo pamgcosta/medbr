39cdb8c153707622bfd59e4b317ae920fb099d97
Pamela Costa <pamelaguimaraescosta@icloud.com>
2023-11-09 22:18:22 -0300

    sixth commit

--- python-package/medbr/data/cities/cities.py
+++ python-package/medbr/data/cities/cities.py
@@ -7,13 +7,14 @@ df = gpd.read_file('medbr/data/cities/raw/BR_Municipios_2022.shp')
 
 df['CD_MUN_CO'] = df.CD_MUN.str[:-1]
 
-df = df[['CD_MUN_CO', 'geometry', 'CD_MUN', 'NM_MUN']].groupby(['CD_MUN_CO', 'geometry', 'NM_MUN']).count().reset_index()
-
-df = df[['CD_MUN_CO', 'geometry', 'NM_MUN']]
+df = df[['CD_MUN_CO', 'geometry', 'NM_MUN', 'SIGLA_UF']]
 
 df = df.rename(columns={
     "CD_MUN_CO": "city_code",
-    "NM_MUN": "city_name"
+    "NM_MUN": "city_name",
+    "SIGLA_UF": "uf"
 })
+
+df['city_code'] = df['city_code'].astype('int64')
 
 df.to_file(agg_file, driver='ESRI Shapefile')
--- python-package/test.py
+++ python-package/test.py
@@ -1,24 +1,28 @@
 import webbrowser
-from medbr import facilities, mortality, birth, plancoverage
+from medbr import facilities, mortality, birth, plancoverage, beds, beneficiaries
 
 
 facilities(city=355030).save('facilities.html')
 webbrowser.open('facilities.html')
 
-beds(city=355030).save('beds.html')
+# no filters allowed
+beds().save('beds.html')
 webbrowser.open('beds.html')
 
-beneficiaries(city=355030).save('beneficiaries.html')
+# no filters allowed
+beneficiaries().save('beneficiaries.html')
 webbrowser.open('beneficiaries.html')
 
 birth(start_at='2021-01-01', end_at='2022-01-01').save('birth.html')
 webbrowser.open('birth.html')
 
-covid_machine(city=355030).save('covid_machine.html')
-webbrowser.open('covid_machine.html')
+# covid_machine(city=355030).save('covid_machine.html')
+# webbrowser.open('covid_machine.html')
 
+# filters start_at and end_at
 mortality(start_at='2021-01-01', end_at='2022-01-01').save('mortality.html')
 webbrowser.open('mortality.html')
 
+# filters by state or operator
 plancoverage(ufs=['SP', 'RJ']).save('plancoverage.html')
 webbrowser.open('plancoverage.html')
